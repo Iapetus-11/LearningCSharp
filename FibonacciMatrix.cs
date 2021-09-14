ad10ff49b896511f4ac9b3f88279b5576c84d34a
Milo Weinberg <iapetus011@gmail.com>
2021-09-14 18:32:58 -0400

    move FibonacciMatrix.cs testing code to a static Test method

--- FibonacciMatrix.cs
+++ FibonacciMatrix.cs
@@ -32,5 +32,17 @@ namespace MyFirstSolution
             BigInteger[] initial = {0, 1, 1, 1};
             return _pow(initial, n)[1];
         }
+
+        public static void Test()
+        {
+            int fibonacciUpTo = 1_000_000;
+            DateTime startTime = DateTime.Now;
+
+            FibonacciMatrix fibonacciMatrixRunner = new FibonacciMatrix();
+            BigInteger result = fibonacciMatrixRunner.Fibonacci(fibonacciUpTo);
+            
+            Console.WriteLine(result);
+            Console.WriteLine($"FibonacciMatrix.fibonacci({fibonacciUpTo}) took {(DateTime.Now - startTime).TotalSeconds} seconds");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -7,14 +7,7 @@ namespace MyFirstSolution
     {
         static void Main(string[] args)
         {
-            int fibonacciUpTo = 1_000_000;
-            DateTime startTime = DateTime.Now;
-
-            FibonacciMatrix fibonacciMatrixRunner = new FibonacciMatrix();
-            BigInteger result = fibonacciMatrixRunner.Fibonacci(fibonacciUpTo);
-            
-            Console.WriteLine(result);
-            Console.WriteLine($"FibonacciMatrix.fibonacci({fibonacciUpTo}) took {(DateTime.Now - startTime).TotalSeconds} seconds");
+            FibonacciMatrix.Test();
         }
     }
 }
