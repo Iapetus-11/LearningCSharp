1284b1676b0d5f804018fd094908511918f4eada
Milo Weinberg <iapetus011@gmail.com>
2021-09-14 18:21:12 -0400

    add Program and FibonacciMatrix

---
+++ FibonacciMatrix.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Numerics;
+
+namespace MyFirstSolution
+{
+    public class FibonacciMatrix
+    {
+        // private BigInteger[] _mat = { 0, 0, 0, 0 };
+
+        private BigInteger[] _mult(BigInteger[] x, BigInteger[] y)
+        {
+            return new BigInteger[]
+            {
+                x[0] * x[1] + x[1] * y[2],
+                x[0] * y[1] + x[1] * y[3],
+                x[2] * y[0] + x[3] * y[2],
+                x[2] * y[1] + x[3] * y[3]
+            };
+        }
+
+        private BigInteger[] _pow(BigInteger[] x, int n)
+        {
+            if (n == 0) return new BigInteger[] { 1, 0, 0, 1 };
+            if (n == 1) return x;
+            if (n % 2 == 0) return _pow(_mult(x, x), n / 2);
+
+            return _mult(x, _pow(x, n - 1));
+        }
+
+        public BigInteger Fibonacci(int n)
+        {
+            BigInteger[] initial = {0, 1, 1, 1};
+            return _pow(initial, n)[1];
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Numerics;
+
+namespace MyFirstSolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fibonacciUpTo = 1_000_000;
+            DateTime startTime = DateTime.Now;
+
+            FibonacciMatrix fibonacciMatrixRunner = new FibonacciMatrix();
+            BigInteger result = fibonacciMatrixRunner.Fibonacci(fibonacciUpTo);
+            
+            Console.WriteLine(result);
+            Console.WriteLine($"FibonacciMatrix.fibonacci({fibonacciUpTo}) took {(DateTime.Now - startTime).TotalSeconds} seconds");
+        }
+    }
+}
