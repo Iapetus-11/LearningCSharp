590b26592badcb046ae51346841640bb5dc0144b
Milo Weinberg <iapetus011@gmail.com>
2021-09-16 16:54:15 -0400

    add Recaman's Sequence implementation

---
+++ RecamanSequence.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LearningCSharp
+{
+    public class RecamanSequence
+    {
+        public static int[] Recaman(int upTo)
+        {
+            int[] result = new int[upTo];
+            HashSet<int> already = new HashSet<int>();
+            already.Add(0);
+
+            for (int i = 1; i < upTo - 1; i++)
+            {
+                int c = result[i - 1] - i;
+
+                if (c < 0 || already.Contains(c)) c = result[i - 1] + i;
+
+                result[i] = c;
+                already.Add(c);
+            }
+
+            return result;
+        }
+
+        public static void Test(int upTo)
+        {
+            Console.Write($"Recaman sequence up to {upTo}: ");
+
+            int[] result = Recaman(upTo);
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.Write(result[i]);
+
+                if (i < result.Length - 1) Console.Write(", ");
+            }
+
+            Console.WriteLine();
+        } 
+    }
+}
