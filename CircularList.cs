f2283703c22ae028f8e0ea442851cb0e48ed67a3
Milo Weinberg <iapetus011@gmail.com>
2021-09-18 12:28:16 -0400

    add CircularList implementation

---
+++ CircularList.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LearningCSharp
+{
+    public class CircularList<T> : List<T>
+    {
+        public new T this[int i] => base[i % base.Count];
+        public new void Insert(int i, T v) => base.Insert(i % base.Count, v);
+        public new void InsertRange(int i, IEnumerable<T> r) => base.InsertRange(i % base.Count, r);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -12,8 +12,16 @@ namespace LearningCSharp
             // new FibonacciMatrix().Test(100_000);
             // PasswordGenerator.Test(100_000, 20);
             // Anagram.Test();
-            new MinecraftBedrockServer("xenon.iapetus11.me", 19132).Test();
+            // new MinecraftBedrockServer("xenon.iapetus11.me", 19132).Test();
             // RecamanSequence.Test(100);
+            
+            // for testing the circular list
+            // var circleList = new CircularList<int>();
+            // for (int i = 0; i < 5; i++) circleList.Add(i);
+            // for (int i = 0; i < circleList.Count * 2; i++) Console.WriteLine(circleList[i]);
+            // circleList.Insert(1, 1);
+            // circleList.Insert(circleList.Count + 2, 1);
+            // for (int i = 0; i < circleList.Count; i++) Console.WriteLine(circleList[i]);
         }
     }
 }
